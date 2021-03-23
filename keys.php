0b0691509a10054634b58caaab06e636b0f56f80
Hasin Hayder <countdraculla@gmail.com>
2021-03-23 17:09:16 +0600

    Links

--- keys.php
+++ keys.php
@@ -2,3 +2,8 @@
 $keys = [];
 $keys['lwhh']='https://learnwith.hasinhayder.com';
 $keys['hg']='https://github.com/hasinhayder';
+$keys['php']='https://learnwith.hasinhayder.com/wp/shop/mastering-php-7/';
+$keys['theme']='https://learnwith.hasinhayder.com/wp/shop/wordpress-theme-development/';
+$keys['plugin']='https://learnwith.hasinhayder.com/wp/shop/wordpress-plugin-development/';
+$keys['wpso']='https://learnwith.hasinhayder.com/wp/shop/wordpress-speed-optimization/';
+$keys['pb']='https://learnwith.hasinhayder.com/wp/shop/wordpress-page-builders/';
